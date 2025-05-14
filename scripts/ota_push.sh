ccef07265d5f5a3c2707b725515ec922f2dee7c7
ChosenCypher <hugs@duck.com>
2025-05-14 22:25:28 +0200

    package_ota and ota_push

--- scripts/ota_push.sh
+++ scripts/ota_push.sh
@@ -7,52 +7,16 @@ cd $DIR
 
 # Constants
 OTA_DIR="$DIR/../output/ota"
-DATA_ACCOUNT="commadist"
+DATA_ACCOUNT="comma"
 
-# Parse input
-FOUND=0
-if [ "$1" == "production" ]; then
-  OTA_JSON="$OTA_DIR/all-partitions.json"
-  DATA_CONTAINER="agnosupdate"
-  FOUND=1
-fi
-if [ "$1" == "staging" ]; then
-  OTA_JSON="$OTA_DIR/all-partitions-staging.json"
-  DATA_CONTAINER="agnosupdate-staging"
-  FOUND=1
-fi
-
-if [ $FOUND == 0 ]; then
-  echo "Supply either 'production' or 'staging' as first argument!"
-  exit 1
-fi
-
-upload_file() {
-  local FILE_NAME=$1
-  local CLOUD_PATH="https://$DATA_ACCOUNT.blob.core.windows.net/$DATA_CONTAINER/$FILE_NAME"
-
-  echo "Copying $FILE_NAME to the cloud..."
-  azcopy cp --log-level ERROR --overwrite=false $OTA_DIR/$FILE_NAME "$CLOUD_PATH?$DATA_SAS_TOKEN"
-  echo "  $CLOUD_PATH"
-}
-
-process_file() {
-  local NAME=$1
-  local HASH_RAW=$(cat $OTA_JSON | jq -r ".[] | select(.name == \"$NAME\") | .hash_raw")
-  upload_file "$NAME-$HASH_RAW.img.xz"
-  if [ -f "$OTA_DIR/$NAME-$HASH_RAW.img" ]; then
-    upload_file "$NAME-$HASH_RAW.img"
-  fi
-}
-
-# Generate token
-echo "Logging in..."
-SAS_EXPIRY=$(date -u '+%Y-%m-%dT%H:%M:%SZ' -d '+1 hour')
-DATA_SAS_TOKEN=$(az storage container generate-sas --as-user --auth-mode login --account-name $DATA_ACCOUNT --name $DATA_CONTAINER --https-only --permissions wr --expiry $SAS_EXPIRY --output tsv)
+OTA_JSON="$OTA_DIR/agnos.json"
+DATA_CONTAINER="agnosupdate"
+VERSION=$(< $DIR/../VERSION)
 
 # Liftoff!
-for name in $(cat $OTA_JSON | jq -r ".[] .name"); do
-  process_file $name
-done
+echo "Copying output/ota to r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION..."
+rclone sync -P "$OTA_DIR" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
+# rclone copy -P "$OTA_DIR" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
+# rclone copy -P "$OTA_DIR" --include "*.img.xz" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
 
 echo "Done!"
