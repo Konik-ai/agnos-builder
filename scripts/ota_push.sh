3a05ac28522b000b2a93d2f2e92d045e415b8882
ChosenCypher <hugs@duck.com>
2025-07-02 21:56:09 +0200

    comma.sh
    Squashed commit of the following:
    
    commit ccb592bc823145bb34325c7abf94068dc475d928
    Author: ChosenCypher <hugs@duck.com>
    Date:   Wed Jun 25 16:38:05 2025 +0200
    
        konik storage
    
    commit b07510758b658c1ba9a935a26e3a9eef4571981e
    Author: ChosenCypher <hugs@duck.com>
    Date:   Mon May 19 09:29:41 2025 +0000
    
        back to stable
    
    commit 981f0cf38ad6a866411d3311c6825a3b87d000b7
    Author: ChosenCypher <hugs@duck.com>
    Date:   Thu May 15 23:07:55 2025 +0200
    
        take care of names and links
    
    commit ccef07265d5f5a3c2707b725515ec922f2dee7c7
    Author: ChosenCypher <hugs@duck.com>
    Date:   Wed May 14 22:25:28 2025 +0200
    
        package_ota and ota_push
    
    commit a0d6ade2d73dc4ea490c8f2ea3e0ae1a08aeef85
    Author: ChosenCypher <hugs@duck.com>
    Date:   Wed May 14 22:26:12 2025 +0200
    
        konik splash
    
    commit ad8ebc849893a5cb533c9ce163b5abf4bb68a0ed
    Author: ChosenCypher <hugs@duck.com>
    Date:   Wed May 14 22:26:00 2025 +0200
    
        take care of qr link
    
    commit efce629a09fa1c79f5f7fdebd3849dbd186ba1eb
    Author: ChosenCypher <hugs@duck.com>
    Date:   Fri May 9 20:40:53 2025 +0000
    
        take care of DongleId
    
    commit 7d7c9088ed0f2ee3ceaf5a1879ae9916604ee9cc
    Author: ChosenCypher <hugs@duck.com>
    Date:   Fri May 9 20:14:07 2025 +0000
    
        comma.sh

---
+++ scripts/ota_push.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# Make sure we're in the correct directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
+
+# Constants
+OTA_DIR="$DIR/../output/ota"
+DATA_ACCOUNT="konik"
+
+OTA_JSON="$OTA_DIR/agnos.json"
+DATA_CONTAINER="agnosupdate"
+VERSION=$(< $DIR/../VERSION)
+
+# Liftoff!
+echo "Copying output/ota to r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION..."
+rclone sync -P "$OTA_DIR" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
+# rclone copy -P "$OTA_DIR" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
+# rclone copy -P "$OTA_DIR" --include "*.img.xz" "r2:$DATA_ACCOUNT/$DATA_CONTAINER/$VERSION"
+
+echo "Done!"
